5b1ebf839f3bf80894cdc2caeb69ff3ebd825bc8
aferica <18819466397@163.com>
2019-06-06 10:33:23 +0800

    修复在线预览

---
+++ dist/precache-manifest.89ee1a59c76c9e64eb8e6554118e4ed2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5fad0677a9c449878ec",
+    "url": "/catalyx/dist/js/login.bbe1ffd4.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/catalyx/dist/robots.txt"
+  },
+  {
+    "revision": "a01ec3e69a01dc45cc34",
+    "url": "/catalyx/dist/js/chunk-vendors.9e65584c.js"
+  },
+  {
+    "revision": "faaf63ca7c3c62b08130",
+    "url": "/catalyx/dist/js/app.ffdebb28.js"
+  },
+  {
+    "revision": "a3e97dc5f4ec92679131",
+    "url": "/catalyx/dist/js/about.9d112481.js"
+  },
+  {
+    "revision": "891031c76c96acf9bb1e6794a6f45506",
+    "url": "/catalyx/dist/index.html"
+  },
+  {
+    "revision": "6e5984df42da9f64da3a96a430f30281",
+    "url": "/catalyx/dist/img/logo.6e5984df.jpg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/catalyx/dist/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/catalyx/dist/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "c5fad0677a9c449878ec",
+    "url": "/catalyx/dist/css/login.837be617.css"
+  },
+  {
+    "revision": "faaf63ca7c3c62b08130",
+    "url": "/catalyx/dist/css/app.2d5f7995.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.aebb05c695bfc76849c7c9e3b429bfcd.js"
+  "/catalyx/dist/precache-manifest.89ee1a59c76c9e64eb8e6554118e4ed2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "catalyx"});
